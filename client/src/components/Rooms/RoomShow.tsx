cd1af5ed15145484a500af9eded7cea4a786ef52
David de Marville <de.marville.david@gmail.com>
2025-08-28 10:01:02 +0200

    add fix images

--- client/src/components/Rooms/RoomShow.tsx
+++ client/src/components/Rooms/RoomShow.tsx
@@ -6,18 +6,18 @@ function RoomShow() {
       <div className="md:flex md:flex-col md:gap-5">
         <div className="md:flex md:justify-center">
           <RoomCarousel
-            images={["chambres/chambre-1.webp", "chambres/chambre-1bis.webp"]}
+            images={["/images/chambres/chambre-1.webp", "/images/chambres/chambre-1bis.webp"]}
           />
         </div>
 
         <div className="md:flex md:gap-5 md:justify-center flex md:mt-0 mt-5 ">
           <img
-            src="chambres/chambre-4.webp"
+            src="/images/chambres/chambre-4.webp"
             alt="room 2"
             className=" md:mr-0 mr-5 md:w-50 w-42 object-cover rounded-2xl "
           />
           <img
-            src="chambres/chambre-5.webp"
+            src="/images/chambres/chambre-5.webp"
             alt="room 3"
             className=" md:w-50 w-42 object-cover rounded-2xl "
           />
